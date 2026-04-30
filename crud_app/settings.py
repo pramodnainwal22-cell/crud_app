424172b16766b7216b82ede811ab2b47afb0d7d1
Pramod Nainwal <pramodnainwal22@gmail.com>
2026-04-29 21:23:52 -0700

    Added .env for security and updated settings

--- crud_app/settings.py
+++ crud_app/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,8 +32,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'pramodnainwal22@gmail.com'
-EMAIL_HOST_PASSWORD = 'xcaf omiq sxpi fzhf'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
